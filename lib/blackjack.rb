2cd804ac4a2f83f652ffb8261d4c861b71cd78a9
Christa Gammage <crgammage@gmail.com>
2020-05-08 15:36:23 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -55,7 +55,6 @@ def runner
   welcome
   card_total = initial_round
   until hit?(card_total) > 21
-    card_total = hit?(card_total)
     display_card_total(card_total)
   end
 end_game(card_total)
